adaa16e80915475659f8c0e20ce757a5bc48e71d
murthyns18 <murthy.ns646@gmail.com>
2026-01-14 22:11:00 +0530

    changes

--- LMS/wwwroot/js/book.js
+++ LMS/wwwroot/js/book.js
@@ -1,12 +1,14 @@
-﻿$(document).ready(function () {
+﻿
+$(document).ready(function () {
     $.ajax({
         url: APIURL + '/Book/BookList?BookId=0',
         type: 'GET',
         dataType: 'JSON',
-        async: false,
-        cache: false,
         success: function (result) {
-            console.log("result", result)
+            console.log("result", result);
+        },
+        error: function (err) {
+            console.error("ajax error", err);
         }
-    })
-})
+    });
+});
